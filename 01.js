5ccebaa189fbb7e6741e7bbca4b5f8fbd98459df
vefa <44091688+veefa@users.noreply.github.com>
2022-11-24 09:15:58 +0100

    Add files via upload

--- 01.js
+++ 01.js
@@ -4,14 +4,17 @@ import * as React from 'react'
   return newState
 }*/
 
-const countReducer = (state, action) => ({...state, ...action}) // merged these two things to get our new state values
+const countReducer = (state, action) => ({...state, 
+  ...(typeof action === 'function' ? action(state) : action)})
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState({count: count + step})
+  //making our reducer support both the object as well as a function callback
+  const increment = () =>
+  setState(currentState => ({count: currentState.count + step})) 
   return <button onClick={increment}>{count}</button>
 } 
 
