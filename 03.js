a07a00cfbce4cd421773c07b27058aae2480a2eb
vefa <44091688+veefa@users.noreply.github.com>
2023-01-31 15:26:16 +0800

    Add files via upload

--- 03.js
+++ 03.js
@@ -1,38 +1,49 @@
-// useContext: simple Counter
-// http://localhost:3000/isolated/exercise/03.js
 
 import * as React from 'react'
 
-// 🐨 create your CountContext here with React.createContext
+//create your CountContext here with React.createContext
+const CountContext = React.createContext();
 
-// 🐨 create a CountProvider component here that does this:
-//   🐨 get the count state and setCount updater with React.useState
-//   🐨 create a `value` array with count and setCount
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
+//create a CountProvider component here that does this:
+const CountProvider = (props) => {
+  //1- get the count state and setCount updater with React.useState
+  const [count, setCount] = React.useState(0);
+  //2- create a `value` array with count and setCount
+  const value = [count, setCount];
+  //3- return your context provider with the value assigned to that array and forward all the other props
 //   💰 more specifically, we need the children prop forwarded to the context provider
 
+  return (
+    <CountContext.Provider value={value} {...props} />
+  );
+};
+
 function CountDisplay() {
-  // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  // get the count from useContext with the CountContext
+  const [count] = React.useContext(CountContext)
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
+  //get the setCount from useContext with the CountContext
+  const [, setCount] = React.useContext(CountContext)
   const increment = () => setCount(c => c + 1)
-  return <button onClick={increment}>Increment count</button>
+  const decrement = () => setCount(c => c - 1)
+  return (   
+    <div>
+      <button onClick={increment}>Increment count</button>
+      <button onClick={decrement}>Decrement count</button>
+    </div>
+  )
 }
 
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
   )
 }
--- Advance React Hooks Note.md
+++ Advance React Hooks Note.md
@@ -304,3 +304,30 @@ class ChildComponent extends React.Component {
   }
 }
 ```
+# Example:
+```javascript
+
+const CountContext = React.createContext();
+
+function ParentComponent() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <CountContext.Provider value={{ count, setCount }}>
+      <ChildComponent />
+    </CountContext.Provider>
+  );
+}
+
+function ChildComponent() {
+  const { count, setCount } = useContext(CountContext);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+```
